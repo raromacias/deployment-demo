8d3dde132c12350498f2d841a056ed83eb1fa5b9
Raro Macias <ggmacias@gmail.com>
2022-09-21 13:55:19 -0400

    boilerplate added

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.listen(port, () => {
+    console.log('Docked at port ' + port)
+})
